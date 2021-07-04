c6269bdbb6f070a88f61288ed5dd6c7139a7c605
Maka998 <correct-marijavucetic@uns.ac.rs>
2021-07-04 17:46:07 +0200

    front

--- Nistagram/user-service/main.go
+++ Nistagram/user-service/main.go
@@ -118,7 +118,7 @@ func handleFunc(handler *handler.RegisteredUserHandler) {
 	router.HandleFunc("/getMyPersonalData/{id}",   IsAuthorized(handler.GetMyPersonalData)).Methods("GET")
 	router.HandleFunc("/changeMyPersonalData/{id}",  IsAuthorized(handler.ChangePersonalData)).Methods("POST")
   router.HandleFunc("/getAccountByUsername/{username}",  IsAuthorized(handler.GetAccountByUsername)).Methods("GET")
-	router.HandleFunc("/searchLocation/{name}", handler.SearchProfile).Methods("GET")
+	router.HandleFunc("/searchProfile/{name}", handler.SearchProfile).Methods("GET")
 
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", os.Getenv("PORT")), router))
 }
